a264421845517c7811276c7a1e6ce38d0d0dac1d
ORazB <devandramahottama@gmail.com>
2026-02-02 21:41:21 +0800

    Add mIddleware logic

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,34 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+import prisma from './lib/prisma';
 
-export default clerkMiddleware();
+import { Role } from './app/generated/prisma';
+
+const isAdminRoute = createRouteMatcher(['/admin(.*)']);
+const isPublicRoute = createRouteMatcher(['/register', '/login', '/api(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (isPublicRoute(req)) return NextResponse.next();
+  
+  if (!userId) return NextResponse.redirect(new URL('/register', req.url));
+
+  const user = await prisma.user.findUnique({
+    where: {clerkId: userId}
+  })
+  
+  if (!user) {
+    return NextResponse.redirect(new URL('/register', req.url));
+  }
+  
+  if (isAdminRoute(req)) {
+    if (user.role !== Role.ADMIN) {
+      return new NextResponse("Forbidden", { status: 403 });
+    }
+  }
+
+});
 
 export const config = {
   matcher: [
