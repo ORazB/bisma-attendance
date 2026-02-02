f746e80cc663a84eb9c4a050d635a2718da60b0c
ORazB <devandramahottama@gmail.com>
2026-02-02 21:42:14 +0800

    Add admin only page

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+import AdminPage from "@/components/Admin/AdminPage";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-zinc-50">
+      <AdminPage />
+    </div>
+  );
+}
